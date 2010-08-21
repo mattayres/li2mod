42e3a7c106317368bd0f951c0c0191f2a737c5aa
Ralf Ertzinger <ralf@skytale.net>
2010-08-21 19:06:24 +0200

    q_shared.c: Fix typo

--- q_shared.c
+++ q_shared.c
@@ -1035,7 +1035,7 @@ char	*va(char *format, ...)
 	static char		string[1024];
 	
 	va_start (argptr, format);
-	vsnprintf (string, sizeof(va_string), format,argptr);
+	vsnprintf (string, sizeof(string), format,argptr);
 	va_end (argptr);
 
 	return string;	
